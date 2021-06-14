460839d0d4852ecca0382760d5daf9bd9d6b6af9
Daniel Malakhov <danielmalakhov@MacBook-Pro-Daniel.local>
2021-06-14 18:41:36 +0300

    2a

--- main.cpp
+++ main.cpp
@@ -1,31 +1,23 @@
-#include <vector>
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+#include "FloatList.cpp"
 
 using namespace std;
 
 int main() {
-    vector<int> list;
-    while (true) {
-        int buf;
-        cin >> buf;
-        if (buf < 0) {
-            break;
-        }
-        if (buf < 10) {
-            list.push_back(buf);
-        }
+    cout << "Running..." << endl;
+    srand(time(NULL));
+    FloatList list;
+    for (int i = 0; i < 10; i++) {
+        float number = i;
+        cout << "Inserting " << number << " into " << i << endl;
+        list.insert(i, number);
+        list.print();
     }
-    int max = 0;
-    int sum = 0;
-    for (int i = 0; i < list.size(); i++) {
-        if (list[i] > max) {
-            max = list[i];
-        }
-        sum += list[i];
-    }
-    cout << "Max: " << max << endl;
-    cout << "Sum: " << sum << endl;
-    cout << "Average: " << sum / list.size() << endl;
+    list.print();
+    list.insert(0, 3.14);
+    list.print();
+    return 0;
 }
-
-
